492db018ba6c4f18fc3b537deea98fbc95ad08a5
jepkess <biivincent1997@gmail.com>
2021-10-06 20:37:24 +0300

    modify the goal component

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -8,23 +8,26 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
   goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date(2020,3,23)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot', new Date(2018,5,14)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon', new Date(2012,7,23)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks', new Date(2014,11,2)),
-    new Goal(5,'Solve math homework','Damn Math', new Date(2016,9,23)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord', new Date(2017,8,8)),
+    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date(2020, 3, 23)),
+    new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot', new Date(2018, 5, 14)),
+    new Goal(3, 'Get new Phone Case', 'Diana has her birthday coming up soon', new Date(2012, 7, 23)),
+    new Goal(4, 'Get Dog Food', 'Pupper likes expensive snacks', new Date(2014, 11, 2)),
+    new Goal(5, 'Solve math homework', 'Damn Math', new Date(2016, 9, 23)),
+    new Goal(6, 'Plot my world domination plan', 'Cause I am an evil overlord', new Date(2017, 8, 8)),
   ];
-  toggleDetails(index:number){
-    this.goals[index].showdescription=!this.goals[index].showdescription;
+  toggleDetails(index: number) {
+    this.goals[index].showdescription = !this.goals[index].showdescription;
   }
-  deleteGoal(isComplete: any, index: number){
+  deleteGoal(isComplete: any, index: number) {
     if (isComplete) {
-      this.goals.splice(index,1);
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+      if (toDelete) {
+        this.goals.splice(index, 1)
+      }
     }
-    
+
   }
-  
+
 
   constructor() { }
 
@@ -38,5 +41,5 @@ export class GoalComponent implements OnInit {
 //   if (isComplete) {
 //     this.goals.splice(index,1);
 //   }
-  
+
 // }
