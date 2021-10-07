cac72753d9fec800d66f6a8f62a5f6bc13627a24
jepkess <biivincent1997@gmail.com>
2021-10-07 13:19:43 +0300

    add bootstrap styles to the form component

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -27,6 +27,12 @@ export class GoalComponent implements OnInit {
     }
 
   }
+  addNewGoal(goal: any){
+    let goalLength= this.goals.length;
+    goal.id= goalLength+1;
+    goal.completeDate= new Date(goal.completeDate);
+    this.goals.push(goal);
+  }
 
 
   constructor() { }
